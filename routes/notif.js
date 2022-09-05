2e0df8cbf6ee0fc0c6ea82d6aad1ac890e11cb47
mikikotech <mikikotech@gmail.com>
2022-09-05 17:51:19 +0800

    test

--- routes/notif.js
+++ routes/notif.js
@@ -8,7 +8,7 @@ var serverKey =
   "AAAAtGXIPyg:APA91bGGAIzPJG9cqkXgme4WG2Lhixfyz54H9_wWh8ZdZbdROG_OcbbTp7DG2nacF8cM_-2Ebwo3pEZucRiTXZOmyD8M4-nb7UxwgWB5Zf4nkWMBVzFqIlkz-_IWbzJqF5s2Dcu9ORnt"; //put your server key here
 var fcm = new FCM(serverKey);
 
-route.post("/", async (req, res) => {
+route.post("/:id", async (req, res) => {
   console.log("notif body = ", req.body);
   try {
     var data = await fire
@@ -17,17 +17,41 @@ route.post("/", async (req, res) => {
       .doc(req.body.email)
       .get();
 
+    var deviceData = await fire
+      .firestore()
+      .collection("devices")
+      .doc(req.params.id)
+      .get();
+
+    const responeData =
+      deviceData._delegate._document.data.value.mapValue.fields;
+
     const fcm_token =
       data._delegate._document.data.value.mapValue.fields.fcm_token.stringValue;
 
     var message = {
       //this may vary according to the message type (single recipient, multicast, topic, et cetera)
       to: fcm_token,
-      // collapse_key: 'your_collapse_key',
 
       notification: {
-        title: req.body.title,
-        body: req.body.msg,
+        title: req.body.type == 1 ? "Schedule Notif" : "Sensor Notif",
+        body:
+          req.body.type == 1
+            ? `${responeData.gardenName.stringValue} ${
+                responeData.switchName.arrayValue.values[req.body.index]
+                  .stringValue
+              } ${req.body.state == 1 ? "ON" : "OFF"}`
+            : `${
+                req.body.index == 0 && req.body.state == 1 // index 0 sensor hujan
+                  ? "Turun Hujan"
+                  : req.body.index == 1 // index 1 sensor PH
+                  ? `PH Tanah ${req.body.state}`
+                  : req.body.index == 2 // index 2 sensor Soil Moisture
+                  ? `Soil Moisture ${req.body.state}`
+                  : req.body.index == 3 // index 3 sensor Temp
+                  ? `Temperature Udara ${req.body.state}`
+                  : `Kelembaban Udara ${req.body.state}` // index 4 sensor Humidity
+              }`,
       },
     };
 
