ae7183e864127b0779054d4cb955281c0aa1bd33
mikikotech <mikikotech@gmail.com>
2022-09-12 19:26:10 +0800

    sensor api

--- routes/sensor.js
+++ routes/sensor.js
@@ -5,37 +5,45 @@ var firebase = require("firebase");
 const route = app.Router();
 
 route.post("/set/:id", async (req, res) => {
-  // console.log(req.body);
+  console.log(req.body);
 
-  await fire
-    .database()
-    .ref(`${req.params.id}/data`)
-    .set({
-      ph: req.body.ph,
-      temp: req.body.temp,
-      humi: req.body.humi,
-      soil: req.body.soil,
-    })
-    .then(() => {
-      res.send("data set");
-    });
+  try {
+    await fire
+      .database()
+      .ref(`${req.params.id}/data`)
+      .set({
+        ph: req.body.ph,
+        temp: req.body.temp,
+        humi: req.body.humi,
+        soil: req.body.soil,
+      })
+      .then(() => {
+        res.send("data set");
+      });
+  } catch (error) {
+    res.status(400).send("error");
+  }
 });
 
 route.post("/add/:id", async (req, res) => {
   // console.log(req.body);
-  await fire
-    .database()
-    .ref(`${req.params.id}/Sensor`)
-    .push()
-    .set({
-      ph: req.body.ph,
-      temp: req.body.temp,
-      humi: req.body.humi,
-      soil: req.body.soil,
-    })
-    .then(() => {
-      res.send("data update");
-    });
+  try {
+    await fire
+      .database()
+      .ref(`${req.params.id}/Sensor`)
+      .push()
+      .set({
+        ph: req.body.ph,
+        temp: req.body.temp,
+        humi: req.body.humi,
+        soil: req.body.soil,
+      })
+      .then(() => {
+        res.send("data update");
+      });
+  } catch (error) {
+    res.status(400).send("error");
+  }
 });
 
 module.exports = route;
